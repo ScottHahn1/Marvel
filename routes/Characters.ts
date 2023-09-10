9017fa48480f3b7da92cbbb78a11f17280edfcf7
Scott Hahn <scotthahn2210@gmail.com>
2023-09-10 11:16:35 +0200

    added folder

---
+++ routes/Characters.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { hash, publicKey, timestamp } from '../functions/api';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    const offset = req.query.offset;
+  
+    axios.get('https://gateway.marvel.com/v1/public/characters?limit=100', {
+      params: {
+        apikey: publicKey,
+        ts: timestamp,
+        hash: hash,
+        offset: offset,
+      }
+    })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(err => console.error(err));
+});
+  
+router.get('/character-info/:id', (req, res) => {
+    const id = req.query.characterId;
+
+    axios.get(`https://gateway.marvel.com/v1/public/characters/${id}`, {
+        params: {
+        apikey: publicKey,
+        ts: timestamp,
+        hash: hash,
+        }
+    })
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(err => console.error(err));
+});
+
+export default router;
