fccfed0ad2609f1a5ab06d90527e56d499533b71
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-27 09:58:18 +0200

    changed api to use api key instead of token. changed sort_by parameter to sort by ascending date instead of descending

--- routes/Movies.ts
+++ routes/Movies.ts
@@ -26,18 +26,17 @@ router.get('/', (req, res) => {
 });
 
 router.get('/featured-movie', (req, res) => {
-    axios.get(`https://api.themoviedb.org/3/discover/movie?&page=1`, {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?&page=1&api_key=${process.env.TMDB_API_KEY}`, {
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
-            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
         },
         params: {
             include_adult: false,
             year: 2025,
             language: 'en-US',
             with_companies: '420|19551|38679|2301|13252',
-            sort_by: 'primary_release_date.desc'
+            sort_by: 'primary_release_date.asc'
         }
     })
     .then(response => {
