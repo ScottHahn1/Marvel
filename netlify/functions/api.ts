16ed30eaeae8b56105e5c85e6a0d3e44fa6b5b45
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-27 09:59:50 +0200

    changed hash function to use crypto.createHash function

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -7,29 +7,25 @@ import { router as comicsRouter } from '../../routes/Comics';
 import { router as moviesRouter } from '../../routes/Movies';
 import { router as seriesRouter } from '../../routes/Series';
 import { router as homeRouter } from '../../routes/Home';
+import crypto from 'crypto';
 
 const app = express();
 app.use(cors({
-  origin: ['https://marvel-world-heroes.netlify.app', 'http://localhost:3000'],
-  methods: 'GET, OPTIONS',
+  origin: 'https://marvel-world-heroes.netlify.app',
+  methods: 'GET',
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 dotenv.config();
 
-const getHash = (ts: string, privateKey: string | undefined, publicKey: string | undefined) => {
-  let md5 = require("md5");
-  return md5(ts + privateKey + publicKey).toString();
-};
-
 export const timestamp = Date.now().toString();
 export const publicKey = process.env.REACT_APP_PUBLIC_API_KEY;
 export const privateKey = process.env.REACT_APP_PRIVATE_API_KEY;
-export const hash = getHash(timestamp, privateKey, publicKey);
+export const hash = crypto.createHash('md5').update(timestamp + privateKey +  publicKey).digest('hex');
 export const tmdbToken = process.env.REACT_APP_BEARER_TOKEN;
 export const newsKey = process.env.REACT_APP_NEWS_KEY;
 
-app.use('/.netlify/functions/api/', homeRouter);
+app.use('/.netlify/functions/api', homeRouter);
 app.use('/.netlify/functions/api/characters', charactersRouter);
 app.use('/.netlify/functions/api/comics', comicsRouter);
 app.use('/.netlify/functions/api/movies', moviesRouter);
